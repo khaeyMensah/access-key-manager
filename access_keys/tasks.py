1fa04a91312d8229e344251c928eba174409e597
Emmanuel K. Mensah <ekmpizarro@gmail.com>
2024-06-23 01:53:43 +0000

    changed user model to custom user model

--- access_keys/tasks.py
+++ access_keys/tasks.py
@@ -1,11 +1,25 @@
 from celery import shared_task
 from django.utils import timezone
 from access_keys.models import AccessKey, KeyLog
-from django.contrib.auth.models import User
-
+from users.models import User 
 
 @shared_task
 def update_key_statuses():
+    """
+    This task updates the status of expired access keys and logs the action.
+
+    Returns:
+        str: A message indicating the completion of the task.
+
+    Raises:
+        Exception: If no admin user is found.
+
+    Example:
+    ```
+    update_key_statuses.delay()
+    ```
+    """
+    
     now = timezone.now().date()
     print(f"Running update_key_statuses at {now}")  # Confirm task execution
     expired_keys = AccessKey.objects.filter(expiry_date__lt=now, status='active')
@@ -29,43 +43,3 @@ def update_key_statuses():
         )
     return "Update completed"
 
-
-
-
-# @shared_task
-# def update_key_statuses():
-#     """
-#     This task updates the status of expired access keys and logs the action.
-
-#     Returns:
-#         str: A message indicating the completion of the task.
-
-#     Raises:
-#         Exception: If no admin user is found.
-
-#     Example:
-#     ```
-#     update_key_statuses.delay()
-#     ```
-#     """
-#     now = timezone.now().date()
-#     print(f"Running update_key_statuses at {now}")
-#     expired_keys = AccessKey.objects.filter(expiry_date__lt=now, status='active')
-#     # system_user = User.objects.filter(is_admin=True).first()
-#     system_user = User.objects.filter(is_superuser=True).first()
-
-
-#     if not system_user:
-#         print('No admin user found. Please create an admin user.')
-#         return
-
-#     for key in expired_keys:
-#         key.status = 'expired'
-#         key.save()
-
-#         KeyLog.objects.create(
-#             access_key=key,
-#             action=f'Access key {key.key} expired for school {key.school.name}',
-#             user=system_user
-#         )
-#     return "Update completed"
